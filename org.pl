790ad42bb69bd04c347707847c3d14a35545a153
Antonio-Melo <happyd3nt@gmail.com>
2016-12-23 01:51:15 +0000

    Checking correctly if there is enough space to every one

--- org.pl
+++ org.pl
@@ -9,33 +9,72 @@ org:-
 	write(' Welcome to Dinner Organizer!!\n By:Antonio Melo & Ariana Fernandes\n\n'),
 	write('Please read the README file before using this program in order to work!\n'),
 	write('Reading Input.txt file...\n'),
-	readFile.
+	readFile(NumberofPersons,NumberofGroups,NumberofDifferentTables,ListofPersons,ListofGroups,ListofTables),
+	write('File read successfully!\n'),
+	write('Number of persons: '),write(NumberofPersons),nl,
+	write('Number of groups: '),write(NumberofGroups),nl,
+	write('Number of different tables: '),write(NumberofDifferentTables),nl,
+	write(ListofPersons),nl,
+	write(ListofGroups),nl,
+	write(ListofTables),nl,
+	
+	checkSeats(NumberofPersons,ListofGroups,ListofTables).
+	
+%--------------checkPlaces--------------------%
+checkSeats(NumberofPersons,ListofGroups,ListofTables):-
+	getNumberofPersonsinGroup(ListofGroups,NumberofPersonsinGroup),
+	write(NumberofPersonsinGroup),nl,
+	getTotalNumberofSeats(ListofTables,NumberofSeats),
+	write(NumberofSeats),nl,
+	TotalNumberofPersons is NumberofPersons + NumberofPersonsinGroup,
+	TotalNumberofPersons > NumberofSeats,
+	write('Not Enough seats to alocate every one...\n').
+	
+checkSeats(NumberofPersons,ListofGroups,ListofTables):-
+	getNumberofPersonsinGroup(ListofGroups,NumberofPersonsinGroup),
+	write(NumberofPersonsinGroup),nl,
+	getTotalNumberofSeats(ListofTables,NumberofSeats),
+	write(NumberofSeats),nl,
+	TotalNumberofPersons is NumberofPersons + NumberofPersonsinGroup,
+	TotalNumberofPersons =< NumberofSeats,
+	write('Enough seats to alocate every one...\n').
 
+%-------------------Groups--------------------%
+getNumberofPersonsinGroup([],0).
+getNumberofPersonsinGroup([G|Gs],NumberofPersonsinGroup):-
+	getNumberofPersonsinGroup(Gs,N2),
+	nth0(0,G,Num),
+	%write(Num).
+	NumberofPersonsinGroup is N2 + Num.
+%-------------------Tables-------------------%
+getTotalNumberofSeats([],0).
+getTotalNumberofSeats([T|Ts],NumberofSeats):-
+	getTotalNumberofSeats(Ts,N2),
+	nth0(0,T,NumberofTableSeats),
+	nth0(1,T,NumberofTables),
+	TempNumber is NumberofTableSeats * NumberofTables,
+	NumberofSeats is N2 + TempNumber.
 
 %--------------------Read File----------------%
-readFile:-
+readFile(NumberofPersons,NumberofGroups,NumberofDifferentTables,ListofPersons,ListofGroups,ListofTables):-
 	open('Input.txt',read,Fd),
 	% Number of Persons %
-  read(Fd,NumberofPersons),
-  write(NumberofPersons),nl,
+	read(Fd,NumberofPersons),
 	number(NumberofPersons),
 
 	% Number of Groups %
 	read(Fd,NumberofGroups),
-	write(NumberofGroups),nl,
+	%write(NumberofGroups),nl,
 	number(NumberofGroups),
 
 	% Number of Different Tables %
 	read(Fd,NumberofDifferentTables),
-	write(NumberofDifferentTables),nl,
+	%write(NumberofDifferentTables),nl,
 	number(NumberofDifferentTables),
 
 	readPersons(Fd,ListofPersons,NumberofPersons),
 	readGroups(Fd,ListofGroups,NumberofGroups),
 	readTables(Fd,ListofTables,NumberofDifferentTables),
-	write(ListofPersons),nl,
-	write(ListofGroups),nl,
-	write(ListofTables),nl,
 
 	close(Fd).
 %-------------------Read Persons--------------%
